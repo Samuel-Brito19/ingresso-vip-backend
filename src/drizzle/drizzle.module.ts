1a704e4de45a3c643d66ef89a45fcee899f46023
Samuel-Brito19 <sambriton7@gmail.com>
2024-12-19 12:44:25 -0300

    Creating users modules

--- src/drizzle/drizzle.module.ts
+++ src/drizzle/drizzle.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 
-@Module({})
+export const DRIZZLE = 'drizzle_connection';
+@Module({
+  providers: [
+    {
+      provide: DRIZZLE,
+      useFactory: (configService: ConfigService) => {
+        const pool = new Pool({
+          connectionString: configService.getOrThrow('DATABASE_URL'),
+        });
+        return drizzle(pool, {
+          schema: {},
+        });
+      },
+      inject: [ConfigService],
+    },
+  ],
+})
 export class DrizzleModule {}
