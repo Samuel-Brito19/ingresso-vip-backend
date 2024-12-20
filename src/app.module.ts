02b1a01b420104fbc27887a797ea8fd0b8724f2e
Samuel-Brito19 <sambriton7@gmail.com>
2024-12-19 22:30:24 -0300

    Creating tickets folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DrizzleModule } from './drizzle/drizzle.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DrizzleModule, UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DrizzleModule, UsersModule, TicketsModule],
   controllers: [],
 })
 export class AppModule {}
