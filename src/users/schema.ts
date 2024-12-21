25ea2ff8bb8eeea727bd91c88e4c5f1589097e33
Samuel-Brito19 <sambriton7@gmail.com>
2024-12-20 21:24:01 -0300

    Create user

--- src/users/schema.ts
+++ src/users/schema.ts
@@ -1,7 +1,12 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { boolean } from 'drizzle-orm/mysql-core';
+import { pgEnum, pgTable, serial, text } from 'drizzle-orm/pg-core';
+
+export const UserRole = pgEnum('userRole', ['ADMIN', 'USER']);
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
+  name: text('name').notNull(),
   email: text('email').unique().notNull(),
   password: text('password').notNull(),
+  role: UserRole('userRole').default('USER').notNull(),
 });
