5814b23a4771b6d552ada812f8638207c6e9886b
Samuel-Brito19 <sambriton7@gmail.com>
2024-12-29 16:42:15 -0300

    JWT configuration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(
+    id: number,
+    password: string,
+  ): Promise<{ access_token: string }> {
+    const user = await this.userService.findUser(id);
+    if (user.password !== password) {
+      throw new UnauthorizedException();
+    }
+    const payload = { sub: user.id, name: user.name };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { DRIZZLE } from 'src/database/database.module';
 import * as schema from './schema';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { eq } from 'drizzle-orm';
 @Injectable()
 export class UsersService {
   constructor(
@@ -14,5 +15,11 @@ export class UsersService {
 
   async createUser(user: typeof schema.users.$inferInsert) {
     await this.database.insert(schema.users).values(user);
+  }
+
+  async findUser(id: number) {
+    return this.database.query.users.findFirst({
+      where: eq(schema.users.id, id),
+    });
   }
 }
