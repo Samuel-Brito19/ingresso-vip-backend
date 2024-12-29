e71471d23ce01a4b1ec011cff2bd98d5c0bb9dad
Samuel-Brito19 <sambriton7@gmail.com>
2024-12-29 15:16:08 -0300

    Authentication files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { EventModule } from './event/event.module';
 import { LocalModule } from './local/local.module';
 import { CategoryModule } from './category/category.module';
 import { PurchaseModule } from './purchase/purchase.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { PurchaseModule } from './purchase/purchase.module';
     LocalModule,
     CategoryModule,
     PurchaseModule,
+    AuthModule,
   ],
   controllers: [],
 })
