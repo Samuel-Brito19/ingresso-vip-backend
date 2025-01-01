addcd8e123bc7d396c2bb0c8a0c82eea8aaebba0
Samuel-Brito19 <sambriton7@gmail.com>
2025-01-01 20:30:17 -0300

    Service functions

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
-
+import { Inject, Injectable } from '@nestjs/common';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { DRIZZLE } from 'src/database/database.module';
+import * as schema from './schema';
+import { eq } from 'drizzle-orm';
 @Injectable()
-export class EventService {}
+export class EventService {
+  constructor(
+    @Inject(DRIZZLE) private readonly database: NodePgDatabase<typeof schema>,
+  ) {}
+
+  async getEvents() {
+    return this.database.query.event.findMany();
+  }
+
+  async createEvent(event: typeof schema.event.$inferInsert) {
+    await this.database.insert(schema.event).values(event);
+    return event;
+  }
+
+  async findByName(name: string) {
+    return this.database.query.event.findFirst({
+      where: eq(schema.event.name, name),
+    });
+  }
+}
